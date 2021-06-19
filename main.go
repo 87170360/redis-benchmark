ed0d688f5bf6e5cd3e5a063589208609898c66bb
liushuxin <liushuxin@yofangame.com>
2021-06-19 15:05:26 +0800

    .

--- main.go
+++ main.go
@@ -29,17 +29,17 @@ func init() {
 }
 
 func main() {
-	write(10000, "len10", generateValue(10))
-	write(50000, "len10", generateValue(10))
-	write(500000, "len10", generateValue(10))
+	write(10000, "len10_10k", generateValue(10))
+	write(50000, "len10_50k", generateValue(10))
+	write(500000, "len10_500k", generateValue(10))
 
-	write(10000, "len1000", generateValue(1000))
-	write(50000, "len1000", generateValue(1000))
-	write(500000, "len1000", generateValue(1000))
+	write(10000, "len1000_10k", generateValue(1000))
+	write(50000, "len1000_50k", generateValue(1000))
+	write(500000, "len1000_500k", generateValue(1000))
 
-	write(10000, "len5000", generateValue(5000))
-	write(50000, "len5000", generateValue(5000))
-	write(500000, "len5000", generateValue(5000))
+	write(10000, "len5000_10k", generateValue(5000))
+	write(50000, "len5000_50k", generateValue(5000))
+	write(500000, "len5000_500k", generateValue(5000))
 
 	analysis()
 }
